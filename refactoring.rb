456d7979e13ec7adbe8ee556d28fba1c1f6e3f46
Kara Capozzi <karacapozzi@Karas-MacBook.local>
2018-02-03 18:54:41 -0500

    refactor form class

---
+++ refactoring.rb
@@ -0,0 +1,21 @@
+<form class="fancy_form">
+
+  <label for="email">email</label>
+  <input type="text" name="email" id="email">
+
+  <label for="username">username</label>
+  <input type="text" name="username" id="username">
+
+  <label for="pin">pin</label>
+  <input type="text" name="pin" id="pin">
+
+  <label for="website">website</label>
+  <input type="text" name="website" id="website">
+
+  <label for="address">address</label>
+  <input type="text" name="address" id="address">
+
+  <label for="alias">alias</label>
+  <input type="text" name="alias" id="alias">
+
+</form>
